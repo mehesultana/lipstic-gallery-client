ab2044d4913c362893cc37d440ba8965816ea357
mehesultana <sultanamehe01@gmail.com>
2021-11-12 23:24:33 +0600

    fix add review

--- src/pages/Dashboard/Review/Review.js
+++ src/pages/Dashboard/Review/Review.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Ratings from '../../Home/Ratings/Ratings';
 import { FcRating } from 'react-icons/fc';
+import './Review.css';
 
 const Review = () => {
 	return (
-		<>
+		<Container className="review">
 			<Ratings />
 			<div className="text-center mt-5 mb-5">
 				<Link to="/addreview">
@@ -15,7 +16,7 @@ const Review = () => {
 					</Button>
 				</Link>
 			</div>
-		</>
+		</Container>
 	);
 };
 
