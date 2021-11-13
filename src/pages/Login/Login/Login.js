7404ceeddb386793aabe3e9f2ccd91bab3fc2ae7
mehesultana <sultanamehe01@gmail.com>
2021-11-13 21:38:54 +0600

    update style

--- src/pages/Login/Login/Login.js
+++ src/pages/Login/Login/Login.js
@@ -22,6 +22,7 @@ const Login = () => {
 	const handleLoginSubmit = (e) => {
 		loginUser(loginData.email, loginData.password, location, history);
 		e.preventDefault();
+		history.push(redirect_uri);
 	};
 
 	const handleGoogleSignIn = () => {
